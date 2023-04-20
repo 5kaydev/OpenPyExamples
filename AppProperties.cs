77cf40cfe01c0f12939945a5d04fb9122486d746
5kaydev <126616561+5kaydev@users.noreply.github.com>
2023-04-20 09:20:33 -0400

    added new

---
+++ AppProperties.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppProperties.Properties
+{
+
+    using System;
+    using System.IO;
+    using System.Xml.Serialization;
+
+    [XmlRoot("config")]
+    public class Config
+    {
+        [XmlElement("database")]
+        public DatabaseConfig[] Databases { get; set; }
+
+        [XmlElement("email")]
+        public EmailConfig Email { get; set; }
+
+        [XmlElement("logging")]
+        public LoggingConfig Logging { get; set; }
+    }
+
+    public class DatabaseConfig
+    {
+        [XmlAttribute("id")]
+        public string Id { get; set; }
+
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("server")]
+        public string Server { get; set; }
+
+        [XmlElement("user")]
+        public string User { get; set; }
+
+        [XmlElement("password")]
+        public string Password { get; set; }
+    }
+
+    public class EmailConfig
+    {
+        [XmlElement("server")]
+        public string Server { get; set; }
+
+        [XmlElement("port")]
+        public int Port { get; set; }
+
+        [XmlElement("user")]
+        public string User { get; set; }
+
+        [XmlElement("password")]
+        public string Password { get; set; }
+    }
+
+    public class LoggingConfig
+    {
+        [XmlElement("level")]
+        public string Level { get; set; }
+
+        [XmlElement("enabled")]
+        public bool Enabled { get; set; }
+
+        [XmlElement("destination")]
+        public string Destination { get; set; }
+
+        [XmlElement("file_path")]
+        public string FilePath { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+using AppProperties.Properties;
+using System.Xml.Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        XmlSerializer serializer = new XmlSerializer(typeof(Config));
+        using (FileStream stream = File.OpenRead(Environment.CurrentDirectory + "\\Properties\\config.xml"))
+        {
+            Config config = (Config)serializer.Deserialize(stream);
+
+            DatabaseConfig database = config.Databases.FirstOrDefault(d => d.Id == "db2");
+
+
+            Console.WriteLine("Database name: " + database.Name);
+            Console.WriteLine("Database server: " + database.Server);
+            Console.WriteLine("Database user: " + database.User);
+            Console.WriteLine("Database password: " + database.Password);
+
+            Console.WriteLine("Email server: " + config.Email.Server);
+            Console.WriteLine("Email port: " + config.Email.Port);
+            Console.WriteLine("Email user: " + config.Email.User);
+            Console.WriteLine("Email password: " + config.Email.Password);
+
+            Console.WriteLine("Logging level: " + config.Logging.Level);
+            Console.WriteLine("Logging enabled: " + config.Logging.Enabled);
+            Console.WriteLine("Logging destination: " + config.Logging.Destination);
+            Console.WriteLine("Logging file path: " + config.Logging.FilePath);
+            Console.ReadLine();
+        }
+    }
+}
